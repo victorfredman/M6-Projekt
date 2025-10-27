ebcb2ad61b49778650f6e9f559b19ddb5bb8af68
WalterBlom <walter-erik.blomberg.2845@student.uu.se>
2025-10-27 16:12:34 +0100

    Update stryktipset.py
    nu kan man lägga bet

--- stryktipset.py
+++ stryktipset.py
@@ -4,10 +4,8 @@ def hämtaData():
     tips = np.loadtxt("topp2.txt", delimiter=',')
     tipsOdds = np.loadtxt("topp2odds.txt", dtype=float, delimiter=',')
     tipsProcent = oddsTillProcent(tipsOdds)
-    beräknaVärde(tipsProcent, tips)
-    print(tipsOdds)
-    print(tips)
-    print(tips[3,2])
+    värdeBets = beräknaVärde(tipsProcent, tips)
+    skrivUt(tipsOdds, tipsProcent, tips, värdeBets)
 
 def oddsTillProcent(odds):
     oddsProcent = np.empty((odds.shape[0], 3))
@@ -41,17 +39,51 @@ def beräknaVärde(odds, folket):
     for i in range(0,odds.shape[0]):
         total = total*högstaVärde[i, 0]
         sannorlikhet = sannorlikhet*högstaVärde[i, 2]/100
-    print(sannorlikhet*100)
-    print(total)
-    print(högstaVärde)
-
-            
-    print(värdeBets)
+    return värdeBets
     
-        
+
+def skrivUt(odds, oddsProcent, folkProcent, värdeBets):
+    kupong = []
+    
+    for i in range(0, odds.shape[0]):
+        print(f"Match {i+1} odds:")
+        for x in range(0,odds.shape[1]):
+            print(odds[i, x]," ", end='')
+        print()
+        print("Sannorlikhet:")
+        for x in range(0,odds.shape[1]):
+            print(f"{round(oddsProcent[i, x], 1)}% ", end='')
+        print()
+        print("Andel bets lagda")
+        for x in range(0,odds.shape[1]):
+            print(f"{folkProcent[i, x]}% ", end='')
+        print()
+        print("förväntat värde på 1X2")
+        for x in range(0,odds.shape[1]):
+            print(f"{round((värdeBets[i, x]-1)*100, 1)}% ", end='')
+        print()
+        print()
+        while True:
+            svar = input("Vill du sätta 1, X eller 2?")
+            if svar != "1" and svar != "2" and svar != "x" and svar != "X":
+                print("skriv 1, X eller 2")
+                continue
+            else:
+                kupong.append(svar)
+                break
+    print(kupong)
+
+"""def hämtaInput(svar):
+    bet = 0
+    if svar == "x" or svar == "X":
+        bet = 1
+    elif svar == "1":
+        bet = 0
+    elif svar == "2":
+        bet = 2
+    else:
+        return -1
+    return bet"""
+
+
 hämtaData()
-    print(värdeBets)
-    
-        
-
-    
